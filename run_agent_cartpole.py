c2e9eabe16ed40d8d5b6b85251491ffc45b5d7e3
zytadam <zytadam@gmail.com>
2024-12-19 14:27:07 -0500

    rainbow result

--- run_agent_cartpole.py
+++ run_agent_cartpole.py
@@ -22,8 +22,8 @@ from envs.cart_pole import CartPoleEnv
 import pickle
 
 def train_agent(agent, num_episodes=500):
-    max_steps = 64
-    batch_size = 128
+    max_steps = 100
+    batch_size = 64
 
     returns = []
     steps = []
@@ -145,7 +145,7 @@ if __name__ == '__main__':
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     print(f"Using device: {device}")
     # agents = ['dqn', 'ddqn', 'multistep_dqn', 'dueling_ddqn', 'prioritized_ddqn', 'distributional_dqn', 'noise_dqn', 'a3c', 'rainbow']
-    agents = ['prioritized_ddqn']
+    agents = ['rainbow']
     results = {}
     for agent_name in agents:
         print(agent_name)
@@ -167,7 +167,7 @@ if __name__ == '__main__':
             elif agent_name == "a3c":
                 agent = A3CAgent(state_dim=state_dim, action_dim=action_dim, device=device)
             elif agent_name == "rainbow":
-                agent = RainbowAgent(state_dim=state_dim, action_dim=action_dim, v_min=0., v_max=100., n_step=1, sigma=0.03, device=device)
+                agent = RainbowAgent(state_dim=state_dim, action_dim=action_dim, v_min=0., v_max=100., n_step=1, sigma=0.017, device=device)
             elif agent_name == "multistep_dqn":
                 agent = MultiStepDQNAgent(state_dim=state_dim, action_dim=action_dim, epsilon_decay=0.99, n_step=4, device=device)
             elif agent_name == "prioritized_ddqn":
@@ -175,7 +175,7 @@ if __name__ == '__main__':
 
             # Execute based on parameters
             if args.train:
-                steps, returns = train_agent(agent, num_episodes=1000)
+                steps, returns = train_agent(agent, num_episodes=300)
                 seeds_returns[SEED] = (steps, returns)
 
             if args.test:
