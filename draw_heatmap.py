b46d4dfea3d24b3490bb5d7cb94e275147ff17f9
kim_jin <kim_jin@yunquna.com>
2024-12-20 22:49:53 -0500

    fix: remove chinese comments

--- draw_heatmap.py
+++ draw_heatmap.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# 读取 CSV 文件
-
 agents = ['dqn', 'ddqn', 'multistep_dqn', 'dueling_ddqn', 'distributional_dqn', 'noise_dqn', 'prioritized_ddqn','dp']
 # agents = ['dp']
 
@@ -12,16 +10,13 @@ for agent in agents:
     df = pd.read_csv(file_path, index_col=0)
     df = df.T
 
-    # 创建热图
     plt.figure(figsize=(10, 8))
     sns.heatmap(df, annot=True, cmap='coolwarm', cbar=True)
 
-    # 添加标题和标签
     plt.title(f'Heatmap of {agent} Value Function')
     plt.xlabel('Columns')
     plt.ylabel('Rows')
 
     plt.savefig(f"figures/heatmaps/{agent}_value_heatmap.png")
 
-    # 显示热图
     plt.show()
